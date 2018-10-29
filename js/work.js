b84c2affbe1ec0e1c592a220e7537dd87221deb9
Justin Dickerson <justin.p.dickerson@gmail.com>
2018-10-28 23:55:38 -0400

    fixed/cleaned up json devicon strings, cleaned up html, fixed css.

--- js/work.js
+++ js/work.js
@@ -3,36 +3,36 @@ var works = [
 		url: "https://youtu.be/PQXwOXdSj9U",
 		title: "Dance Products",
 		code: "<a href='https://github.com/dj911ice/nameofapp' target='_blank'><i class='devicon-github-plain-wordmark'></i></a>",
-		tech: "<i class='devicon-ruby-plain-wordmark colored'></i> <i class='devicon-rails-plain-wordmark colored'></i>\
-		<i class='devicon-html5-plain-wordmark'></i> <i class='devicon-css3-plain-wordmark'></i> <i class='devicon-javascript-plain'></i>\
-		<i class='devicon-jquery-plain-wordmark'></i> <i class='devicon-bootstrap-plain-wordmark'></i> <i class='devicon-postgresql-plain-wordmark'></i> <i class='devicon-redis-plain-wordmark'></i>",
+		tech: "<i class='devicon-ruby-plain-wordmark tech-icon'></i> <i class='devicon-rails-plain-wordmark tech-icon'></i>\
+		<i class='devicon-html5-plain-wordmark tech-icon'></i> <i class='devicon-css3-plain-wordmark tech-icon'></i> <i class='devicon-javascript-plain tech-icon'></i>\
+		<i class='devicon-jquery-plain-wordmark tech-icon'></i> <i class='devicon-bootstrap-plain-wordmark tech-icon'></i> <i class='devicon-postgresql-plain-wordmark tech-icon'></i> <i class='devicon-redis-plain-wordmark tech-icon'></i>",
 		pic: "img/mytestappjpd-sm.png"
 	},
 	{
 		url: "https://youtu.be/Z3_WHjstaDU",
 		title: "Cabinet App",
 		code: "<a href='https://github.com/dj911ice/cabinet' target='_blank'><i class='devicon-github-plain-wordmark'></i></a>",
-		tech: "<i class='devicon-ruby-plain-wordmark colored'></i> <i class='devicon-rails-plain-wordmark colored'></i>\
-		<i class='devicon-html5-plain-wordmark'></i> <i class='devicon-css3-plain-wordmark'></i> <i class='devicon-javascript-plain'></i>\
-		<i class='devicon-bootstrap-plain-wordmark'></i>",
+		tech: "<i class='devicon-ruby-plain-wordmark tech-icon'></i> <i class='devicon-rails-plain-wordmark tech-icon'></i>\
+		<i class='devicon-html5-plain-wordmark tech-icon'></i> <i class='devicon-css3-plain-wordmark tech-icon'></i> <i class='devicon-javascript-plain tech-icon'></i>\
+		<i class='devicon-bootstrap-plain-wordmark tech-icon'></i>",
 		pic: "img/cabinet_ror.png"
 	},
 	{
 		url: "https://youtu.be/eyWo9FHxzB4",
 		title: "Habitica Abroad",
 		code: "<a href='https://github.com/dj911ice/HabiticalTravel' target='_blank'><i class='devicon-github-plain-wordmark'></i></a>",
-		tech: "<i class='devicon-csharp-plain-wordmark'></i> <i class='devicon-dot-net-plain-wordmark colored'></i>\
-		<i class='devicon-html5-plain-wordmark'></i> <i class='devicon-css3-plain-wordmark'></i> <i class='devicon-javascript-plain'></i>\
-		<i class='devicon-jquery-plain-wordmark'></i> <i class='devicon-bootstrap-plain-wordmark'></i> <i class='devicon-mysql-plain-wordmark'></i>\
-		<i class='devicon-visualstudio-plain-wordmark'></i>",
+		tech: "<i class='devicon-csharp-plain-wordmark tech-icon'></i> <i class='devicon-dot-net-plain-wordmark tech-icon'></i>\
+		<i class='devicon-html5-plain-wordmark tech-icon'></i> <i class='devicon-css3-plain-wordmark tech-icon'></i> <i class='devicon-javascript-plain tech-icon'></i>\
+		<i class='devicon-jquery-plain-wordmark tech-icon'></i> <i class='devicon-bootstrap-plain-wordmark tech-icon'></i> <i class='devicon-mysql-plain-wordmark tech-icon'></i>\
+		<i class='devicon-visualstudio-plain-wordmark tech-icon'></i>",
 		pic: "img/HabiticaAbroad_dashboad.PNG"
 	},
 	{
 		url: "https://youtu.be/FdY97Lx7Vo4",
 		title: "GC Library",
 		code: "<a href='https://github.com/dj911ice/grandcircus_midterm2018' target='_blank'><i class='devicon-github-plain-wordmark'></i></i></a>",
-		tech: "<i class='devicon-csharp-plain-wordmark'></i> <i class='devicon-dot-net-plain-wordmark colored'></i>\
-		<i class='devicon-visualstudio-plain-wordmark'></i>",
+		tech: "<i class='devicon-csharp-plain-wordmark tech-icon'></i> <i class='devicon-dot-net-plain-wordmark tech-icon'></i>\
+		<i class='devicon-visualstudio-plain-wordmark tech-icon'></i>",
 		pic: "img/GCLibrary_sc.PNG"
 	},
 ];
